3ed32d2b76e97f8128453c2f1fa2938107f53ffd
YuanYayi <callmecrystal@163.com>
2018-12-06 16:36:13 +0800

    Update apply-bind.js
    可以增加参数了

--- apply-bind.js
+++ apply-bind.js
@@ -4,8 +4,8 @@ let b = {
 
 function Person(name) {
   this.name = name
-  this.sayName = function() {
-    console.log(this.name)
+  this.sayName = function(words='Hello') {
+    console.log(words+', '+this.name+'!')
   }
 }
 
@@ -13,25 +13,25 @@ var a = new Person('Anne')
 
 // 正常的使用：
 a.sayName()
-a.sayName.apply(b);
+a.sayName.apply(b,['Hi']);
 var fn = a.sayName.bind(b);
-fn();
+fn('Hi');
 
 // 用call, apply实现bind
 // 1. 写个方法
 console.log('--------------------')
 function applyBind(fn, obj) {
-  return () => { fn.apply(obj) }
+  return () => { fn.apply(obj, [].slice.call(arguments, 2)) }
 }
 
-var callB = applyBind(a.sayName, b)
+var callB = applyBind(a.sayName, b, 'Good Morning')
 callB()
 
 // 2.挂载到Function原型上：
 console.log('--------------------')
 Function.prototype.applyBind = function(obj){
-  return ()=>{this.apply(obj)}
+  return ()=>{this.apply(obj, [].slice.call(arguments, 1))}
 }
 
-var callB = a.sayName.applyBind(b)
+var callB = a.sayName.applyBind(b, 'Nice to meet you')
 callB()
